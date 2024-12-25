4b2178ee8c14dd9eba2b45c3ed85cf8dd6a19ec4
Tanaton_Phuk <fifa52766@gmail.com>
2024-12-26 01:03:09 +0700

    add blagitem and list

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -7,16 +7,22 @@ const Header = () => {
         <div className="py-5 px-5 md:px-12 lg:px-28">
             <div className="flex justify-between items-center">
                 <Image src={assets.logo} width={180} alt='' className="w-[130px] sm:w-auto"/>
-                <button className="flex items-center gap-2 font-medium py-1 px-3 sm:px-6 sm:py-3 border border-solid border-black shadow-[-7px_7px_0px_#000000]">Get start <Image src={assets.arrow} ></Image></button>
+                <button className="flex items-center gap-2 font-medium py-1 px-3 sm:px-6 sm:py-3 border border-solid border-black shadow-[-7px_7px_0px_#000000]">Get start <Image src={assets.arrow} alt='' ></Image></button>
                            
             </div>
             <div className="flex justify-between item-center border-black border-solid">
                 <div className=" my-8 ">
                     <h1 className="text-3xl sm:text-5xl font-medium">Last Blogs</h1>
                     <p className="mt-1 mb-5 max-w-[740px] m-auto text-xs sm:text-base">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel natus ullam perferendis ut ratione eum asperiores sunt quibusdam nisi quam?</p>
-                    <button className="flex items-center gap-2 font-medium py-1 px-3 sm:px-6 sm:py-3 border border-solid border-black ">Get start <Image src={assets.arrow} ></Image></button>
+                    <button className="flex items-center gap-2 font-medium py-1 px-3 sm:px-6 sm:py-3 border border-solid border-black ">Get start <Image src={assets.arrow} alt=''></Image></button>
                 </div>
                 <Image src={assets.test} width={700} alt='' className="border w-[160px] sm:h-auto sm:w-auto"/>
+            </div>
+            <div className="flex justify-between item-center border-black border-solid">
+             <form action="" className="flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]">
+                <input type="email" placeholder="Enter your Email" className="pl-4 outline-none"/>
+                <button type="submit" className="border-l border-end border-black py-4 px-4 sm8:px-8 active:bg-gray-600 active:text-white">Subscribe</button>
+             </form>
             </div>
         </div>
     )
--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
+"use client"
+// import BlogItem from "@/Components/BlogItem";
+import BlogList from "@/Components/BlogList";
 import Header from "@/Components/Header";
 
 export default function Home() {
   return (
     <>
     <Header/>
+    <BlogList/>
     </>
   );
 }
